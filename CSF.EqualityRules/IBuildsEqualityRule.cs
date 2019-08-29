0dfcc1412021b478b2a0888ef52e9a54a1feead7
Craig Fowler <craig.fowler@tracesolutions.co.uk>
2019-08-29 13:41:22 +0100

    First work on builder

---
+++ CSF.EqualityRules/IBuildsEqualityRule.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace CSF.EqualityRules
+{
+    public interface IBuildsEqualityRule<T>
+    {
+        ISet<PropertyInfo> Properties { get; }
+        ISet<FieldInfo> Fields { get; }
+        ICollection<IEqualityRule<T>> Rules { get; }
+    }
+}
---
+++ CSF.EqualityRules/RuleBuildingHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace CSF.EqualityRules
+{
+    public class RuleBuildingHelper<T> : IBuildsEqualityRule<T>
+    {
+        readonly ISet<PropertyInfo> properties;
+        readonly ISet<FieldInfo> fields;
+        readonly ICollection<IEqualityRule<T>> rules;
+
+        ISet<PropertyInfo> IBuildsEqualityRule<T>.Properties => properties;
+
+        ISet<FieldInfo> IBuildsEqualityRule<T>.Fields => fields;
+
+        ICollection<IEqualityRule<T>> IBuildsEqualityRule<T>.Rules => rules;
+
+        public RuleBuildingHelper()
+        {
+            properties = new HashSet<PropertyInfo>();
+            fields = new HashSet<FieldInfo>();
+            rules = new List<IEqualityRule<T>>();
+        }
+    }
+}
